c5d4df26a7958dafae934a375cadaedc7c16f1dc
0417keito <117526884+0417keito@users.noreply.github.com>
2023-04-19 23:55:09 +0900

    Update run_python.js

--- run_python.js
+++ run_python.js
@@ -30,38 +30,3 @@ async function runPythonScript(filePath) {
 
 module.exports = { runPythonScript }
 
-//--q このコードは具体的に何をしているんですか？
-//--a このコードは、Pythonスクリプトを実行しています。
-//--q 具体的にはどのような操作を行っているんですか？順番に教えてください。
-//--a このコードは、以下のような操作を行っています。
-//--a 1. Pythonスクリプトを実行する。
-//--a 2. 標準出力を取得する。
-//--a 3. 標準エラー出力を取得する。
-//--a 4. 実行完了時の処理を行う。
-//--q python.on('close', (code) => {の中身はどういう意味ですか？
-//--a python.on('close', (code) => {の中身は、Pythonスクリプトの実行結果を取得しています。
-//--q python.on('close', (code) => {のどこで実行結果はどこで取得されていますか？
-//--q この関数はどうやって使うんですか？また戻り値を変数に格納は出出来ますか？
-//--a この関数は、以下のように使います。
-//--a const filepath = await runPythonScript(filePath);
-//--a また、戻り値を変数に格納することが出来ます。
-//--q この戻り値のfilepathは、実行するpythonファイルがrunning.pyの時どうなりますか？
-//--a この戻り値のfilepathは、実行するpythonファイルがrunning.pyの時、  
-//--a 以下のようになります。
-//--a Python script executed successfully.
-//--a また、この戻り値のfilepathは、実行するpythonファイルがrunning.pyの時、
-//--a 以下のようになります。
-//--a Python script execution failed with code 1.
-//--q つまり、この関数を使うにはどうすればいいですか？また戻り値を変数に格納は出出来ますか？
-//--a この関数を使うには、以下のようにします。
-//--a const filepath = await runPythonScript(filePath);
-//--a また、戻り値を変数に格納することが出来ます。
-//--q この時、filepathはどうなりますか？
-//--a この時、filepathは、以下のようになります。
-//--a Python script executed successfully.
-//--q const filepath = await runPythonScript(filePath);のconst filepathは何が格納されていますか？
-//--a const filepath = await runPythonScript(filePath);のconst filepathは、 
-//--a Python script executed successfully.が格納されています。
-//--q つまり、const filepathには文字列が格納されているんですか？
-//--a つまり、const filepathには文字列が格納されているんです。
-
